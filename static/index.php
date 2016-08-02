6979973c0ac0dd7a5ad891a6222761c48df63f47
davecthomas <davidcthomas@gmail.com>
2016-08-02 10:00:14 -0500

    cors

--- static/index.php
+++ static/index.php
@@ -1,5 +1,6 @@
 <?php
-$url = "http://ajj3.herokuapp.com/index.html";
+ header("Access-Control-Allow-Origin: *");
+ $url = "http://ajj3.herokuapp.com/index.html";
 $statusCode = 301;
 header('Location: ' . $url, true, $statusCode);
 ?>
